4713d8c24900588e0ad2b6f1f9ca119d3bd5a8d1
xsimov <xsimov@gmail.com>
2014-07-21 22:41:53 +0200

    Initial commit

---
+++ lib/brisca.rb
@@ -0,0 +1,67 @@
+require 'pry'
+
+WHO_WINS = [2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 3, 1]
+
+class Deck
+
+	attr_accessor :all_deck, :drawn
+
+	def initialize
+		@all_deck = []
+		@drawn = []
+		(1..12).to_a.each do |num|
+			[:orus, :espases, :copes, :bastos].each do |pal|
+				@all_deck << Hash.new(:num => num, :pal => pal)
+			end
+		end
+	end
+
+	def draw_a_card
+		n = rand(48)
+		return "No more cards in this deck!" if @drawn.length == 48
+		unless @drawn.include? n
+			final_card = @all_deck[n]
+			@drawn.push(n)
+		else
+			final_card = draw_a_card
+		end
+		final_card
+	end
+end
+
+class Round
+
+	def same_pal
+		@played_cards.each do |card|
+			card["rank"] = WHO_WINS.index card[:num]
+		end
+		@played_cards.sort! {|f, s| s["rank"] <=> f["rank"]}
+		"Player #{@played_cards[0]["player"]}"
+	end
+
+	def self.ruling_pal(pal)
+		@@ruling_pal = pal
+	end
+
+	def play(*cards)
+		@played_cards = []
+		players = ["A", "B", "C", "D"]
+		cards.each.with_index do |card, i|
+			card["player"] = players[i]
+			@played_cards.push(card)
+		end
+		all_the_same = true
+		all_pals = [@played_cards[0][:pal]]
+		@played_cards.each do |card|
+			all_the_same = false unless all_pals.include? card[:pal]
+			all_pals.push(card[:pal])
+		end
+		return same_pal if all_the_same
+		return dif_pal
+	end
+
+	def dif_pal
+		# if @second[:pal] == @@ruling_pal
+	end
+
+end
---
+++ spec/brisca_spec.rb
@@ -0,0 +1,56 @@
+require_relative '../lib/brisca'
+
+describe "the card deck" do
+
+	before(:each) do
+		@current_deck = Deck.new
+	end
+
+	it "has 48 cards" do
+		expect(@current_deck.all_deck.length).to eq(48)
+	end
+
+	it "never draws the same card twice" do
+		@drawn_cards = []
+		48.times{
+			@drawn_cards.push(@current_deck.draw_a_card)
+		}
+		check = true
+		@current_deck.all_deck.each do |card|
+			check=false unless @drawn_cards.include? card
+		end
+		expect(check).to eq(true)
+	end
+end
+
+describe "the 2 players game round" do
+
+	it "is won by the highest number, counting 1,3,12,11...4,2" do
+		card1 = {num: 3, pal: :orus}
+		card2 = {num: 1, pal: :orus}
+		expect(Round.new.play(card1, card2)).to eq("Player B")
+	end
+
+	it "is won by the first pal played if niether are the ruling pal" do
+		card1 = {num: 9, pal: :copes}
+		card2 = {num: 1, pal: :orus}
+		this_round = Round.ruling_pal(:espases)
+		this_round = Round.new.play(card1, card2)
+		expect(this_round).to eq("Player A")
+	end
+end
+
+describe "the 3 players game round" do
+	before(:each) do
+		@deck1 = Deck.new
+	end
+
+	it "takes three cards of the same pal and outputs the rank winner" do
+		card1 = {num: 9, pal: :orus}
+		card2 = {num: 1, pal: :orus}
+		card3 = {num: 2, pal: :orus}
+		this_round = Round.ruling_pal(:copes)
+		this_round = Round.new.play(card1, card2, card3)
+		expect(this_round).to eq("Player B")
+	end
+end
