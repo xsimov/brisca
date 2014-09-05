22b3a9e372bfb78c4e56ca0a142fa021ecadfb92
xsimov <xsimov@gmail.com>
2014-09-05 20:05:35 +0200

    Red

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,11 +2,11 @@ describe "the game" do
   context "basics" do
 
     before :each do
-      deck = Deck.new
+      @deck = Deck.new
     end
 
     it "accepts a deck to start a game" do
-      expect(Game.new deck).to be_a(Game)
+      expect(Game.new @deck).to be_a(Game)
     end
   end
 end
