b1caf1475a4d4c8a99340a21910e210fb6fc2545
xsimov <xsimov@gmail.com>
2014-09-05 20:09:14 +0200

    Green

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,18 @@
 describe "the game" do
-  context "basics" do
+  context "basics to start a game" do
 
     before :each do
       @deck = Deck.new
+      @players = []
+      2.times { @players << Player.new @deck }
     end
 
-    it "accepts a deck to start a game" do
+    it "accepts a deck" do
       expect(Game.new @deck).to be_a(Game)
+    end
+
+    it "accepts 2 players" do
+      expect(Game.new @deck, @players)
     end
   end
 end
