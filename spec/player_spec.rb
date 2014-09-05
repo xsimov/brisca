1fd00fccd1daebf7973db008035b077071cd4f62
xsimov <xsimov@gmail.com>
2014-09-05 20:15:03 +0200

    Red

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,37 +2,44 @@ require 'game'
 
 describe "the player object" do
 
-  before(:each) do
-    @deck = Deck.new
-    Round.ruling_suite(@deck.draw_a_card.suite)
-    @daniel = Player.new @deck
-    @daniels_hand = []
-    @daniel.shows_all_hand.each do |card|
-      @daniels_hand << card
+  it "can have a deck linked to it" do
+    daniel = Player.new
+    this_deck = Deck.new
+    expect(daniel.assign_deck this_deck).to eq(this_deck)
+  end
+
+  context "upon having a deck assigned" do
+    before(:each) do
+      @deck = Deck.new
+      @daniel = Player.new @deck
+      @daniels_hand = []
+      @daniel.shows_all_hand.each do |card|
+        @daniels_hand << card
+      end
     end
-  end
 
-  it "has a three-something hand at the beginning" do
-    expect(@daniel.shows_all_hand.length).to eq(3)
-  end
-
-  it "has cards in its hand" do
-    @daniel.shows_all_hand.each do |card|
-      expect(card).to be_a(Card)
+    it "has a three-something hand at the beginning" do
+      expect(@daniel.shows_all_hand.length).to eq(3)
     end
-  end
 
-  it "chooses from its hand when asked to play" do
-    expect(@daniels_hand).to include(@daniel.play_a_card)
-  end
+    it "has cards in its hand" do
+      @daniel.shows_all_hand.each do |card|
+        expect(card).to be_a(Card)
+      end
+    end
 
-  it "the card it chose disappears from its hand" do
-    played_card = @daniel.play_a_card
-    expect(@daniel.shows_all_hand).not_to include(played_card)
-  end
+    it "chooses from its hand when asked to play" do
+      expect(@daniels_hand).to include(@daniel.play_a_card)
+    end
 
-  it "still has 3 cards on its hand after playing" do
-    @daniel.play_a_card
-    expect(@daniel.shows_all_hand.count).to eq(3)
+    it "the card it chose disappears from its hand" do
+      played_card = @daniel.play_a_card
+      expect(@daniel.shows_all_hand).not_to include(played_card)
+    end
+
+    it "still has 3 cards on its hand after playing" do
+      @daniel.play_a_card
+      expect(@daniel.shows_all_hand.count).to eq(3)
+    end
   end
 end
